9dc992cfa90b94c6d6534f247a9c86d44c8388e7
Eric M <emarvets@gmail.com>
2019-12-28 20:35:57 -0500

    Initial template creation

---
+++ Plugin/PluginCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace Plugin
+{
+    public class PluginCommand : Command
+    {
+        public PluginCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static PluginCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "HelloWorld"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("Hello World");
+            RhinoApp.WriteLine(@"<o>");
+
+            return Result.Success;
+        }
+    }
+}
---
+++ PluginShim/PluginShim.cs
@@ -0,0 +1,25 @@
+﻿namespace PluginShim
+{
+
+    #region Don't touch, you only need one
+
+    public class PluginShim : Rhino.PlugIns.PlugIn
+
+    {
+        public PluginShim()
+        {
+            Instance = this;
+        }
+
+
+        public static PluginShim Instance
+        {
+            get; private set;
+        }
+
+    
+    }
+    #endregion
+
+
+}
